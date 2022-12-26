c301a9ba0b5a70de321a78308cbedc24fe2f3243
Kirill <kovrygakirill@mail.ru>
2022-12-26 14:00:15 +0300

    refactoring models

--- dictionary/models.py
+++ dictionary/models.py
@@ -2,25 +2,25 @@ from django.db import models
 from loginsys.models import UserProfile
 
 
-class Dictionary(models.Model):
+class Word(models.Model):
     name = models.CharField(max_length=30)
-    user_id = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL, related_name='dictionaries')
+    transcript = models.CharField(max_length=30, blank=True)
+    ru_translate = models.CharField(max_length=30)
 
     class Meta:
-        db_table = "dictionary"
+        db_table = "word"
 
     def __str__(self):
         return self.name
 
 
-class Word(models.Model):
+class Dictionary(models.Model):
     name = models.CharField(max_length=30)
-    transcript = models.CharField(max_length=30, blank=True)
-    ru_translate = models.CharField(max_length=30)
-    dictionaries = models.ManyToManyField(Dictionary, related_name='words')
+    user_id = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL, related_name='dictionaries')
+    words = models.ManyToManyField(Word, related_name='dictionaries')
 
     class Meta:
-        db_table = "word"
+        db_table = "dictionary"
 
     def __str__(self):
         return self.name
